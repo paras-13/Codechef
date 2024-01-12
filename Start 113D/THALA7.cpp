050a0f84a19575da766a083bea4a0efd596de325
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-01-12 23:56:57 +0530

    THALA7.cpp

---
+++ Start 113D/THALA7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if(n==7) cout << "THALA" << endl;
+    else cout << "SADGE" << endl;
+	// your code goes here
+	return 0;
+}
