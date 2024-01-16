3aba3e2b49667508facece08b269e7c990dc8bb3
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-01-16 23:46:28 +0530

    CHRISTGREET

---
+++ Start 114D/CHRISTGREET.cpp
@@ -0,0 +1,13 @@
+// Start 114 D
+// Ques 1 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+    int n;
+    cin >> n;
+    if(n == 25) cout << "CHRISTMAS" << endl;
+    else cout << "ORDINARY" << endl;
+}
