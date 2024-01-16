fd9ff1213a4211bc6b38535ac8f3755a278bfa03
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-01-16 23:46:59 +0530

    PAINTCHRIS.cpp

---
+++ Start 114D/PAINTCHRIS.cpp
@@ -0,0 +1,22 @@
+// Start 114 D
+// Ques 2 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    int x, y, z;
+    cin >> t;
+    while(t--)
+    {
+        int count = 0;
+        cin >> x >> y >> z;
+        int area = x*y;
+        int cost = area*2;
+        cout << z/cost << endl;
+        
+    }
+	// your code goes here
+
+}
