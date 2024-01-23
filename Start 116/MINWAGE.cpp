2e1d02d43030a5dcfad418038c78507d707905d2
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-01-23 23:28:05 +0530

    MINWAGE

---
+++ Start 116/MINWAGE.cpp
@@ -0,0 +1,11 @@
+// START 116
+// Ques 1 Solution
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    if(x>11) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
